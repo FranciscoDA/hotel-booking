85f76949854367c0f4e54b86a704565b5fe04344
JBeCast <jbcsound@gmail.com>
2014-05-14 19:41:36 -0300

    Initial commit

---
+++ booking/models.py
@@ -0,0 +1,42 @@
+from django.db.models import *
+
+# Model, CharField, TextField,\
+# PositiveSmallIntegerField, EmailField, BooleanField, DateField,\
+# DateTimeField, IntegerField, ForeignKey
+
+
+class Client(Model):
+    def __str__(self):
+        return self.name
+    name = CharField('Nombre', max_length=50)
+    # dni = IntegerField('DNI', unique=True)
+    # email = EmailField('e-mail')
+    # phone = IntegerField('Tel')
+
+
+class Booking(Model):
+    # Por el momento, Localizador = id. Ideal: base 32.
+    def __str__(self):
+        return 'r' + str(self.id)
+    client = ForeignKey('Client', verbose_name='Cliente')
+    room = ForeignKey('Room', verbose_name='Habitación')
+    booked = DateTimeField('Efectuada', auto_now=True)
+    checkin = DateField('Check-in')
+    checkout = DateField('Check-out')
+    # pax = PositiveSmallIntegerField(default=1)
+    # clickon = BooleanField('clickOn', default=False)
+    # price = IntegerField('Precio')
+    # car = BooleanField('Coche', default=False)
+    # obs = TextField('Obs')
+
+class Night(Model):
+    pass
+
+
+class Room(Model):
+    def __str__(self):
+        return str(self.number)
+    number = PositiveSmallIntegerField('Núm.', primary_key=True)
+    # type = CharField('Tipo', max_length=50)
+    # obs = CharField('Obs')
+
