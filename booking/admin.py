71bf37e793fdccbceade69b192d2b5e2b6e7fc12
JBeCast <jbcsound@gmail.com>
2014-05-15 22:27:56 -0300

    Modified ideas

--- booking/admin.py
+++ booking/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from booking.models import *
 
+
+class NightInline(admin.StackedInline):
+    model = Night
+
+
+class BookingAdmin(admin.ModelAdmin):
+    inlines = [NightInline]
+
+
 admin.site.register(Room)
 admin.site.register(Client)
-admin.site.register(Booking)
+admin.site.register(Booking, BookingAdmin)
